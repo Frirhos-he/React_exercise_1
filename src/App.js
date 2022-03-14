9efb92e983d8821867609e1e5b422c8308e42e91
bruno <bruno2001@hotmail.it>
2022-03-14 19:15:53 +0100

    Prima versione funzionante

--- src/App.js
+++ src/App.js
@@ -1,29 +1,66 @@
 import "./styles.css";
-import { useState } from "react";
+import {useCallback, useMemo, useState} from "react";
 
 import ChallengeDescription from "./challengeDescription";
 import ExpensesCard from "./expensesCard";
+//imported libraries
+/*with redux
+import { actions, selectors } from "../../redux-store";
+import { useDispatch, useSelector } from "react-redux";
+ */
+//without redux
 
 export default function App() {
-  const [expenses, setExpenses] = useState([15.4, 20]);
+  const [expenses, setExpenses] = useState([]);
 
   /* SCRIVI QUI IL TUO CODICE */
+    const [item, setNewItem] = useState('');
 
-  return (
+    const onSubmit = useCallback((e) => {
+
+      e.preventDefault();
+      console.log(typeof item);
+
+      const number = parseFloat(item);
+      console.log(number);
+
+      if(!isNaN(number))
+        setExpenses([...expenses, number]); //if number was the one I meant to change I would have put => to directly modify its value with
+      //no other dependencies
+
+        setNewItem('');
+
+    }, [item]);
+
+
+    const handleAverage = useMemo(() => {
+
+      const average = arr => arr.reduce((a,b) => a + b, 0) / arr.length;
+      return average(expenses).toFixed(2).replace(".", ",");
+
+    },[expenses]);
+
+    const handleItemChange = useCallback((event) => {
+        setNewItem(event.target.value); //item is the text written in real time
+    }, []);
+
+
+
+    return (
     <div className="App">
       <ChallengeDescription />
       <div>
-        <form>
-          <input name="expense" />
+        <form onSubmit={onSubmit}>
+          <input name="expense" value={item} placeholder="Quanti paperoni hai speso..." onChange={handleItemChange}/>
           <button type="submit">Aggiungi</button>
         </form>
       </div>
       <div>
-        <h2>Media: ...</h2>
+        <h2>Media:{handleAverage}</h2>
       </div>
       <h2>Spese</h2>
-      {expenses.map((amount) => (
-        <ExpensesCard amount={amount} />
+      {expenses.map((amount,index) => (
+        <ExpensesCard key={index} amount={amount} />
       ))}
     </div>
   );
--- src/expensesCard.js
+++ src/expensesCard.js
@@ -2,8 +2,9 @@ export default (props) => {
   const { amount } = props;
 
   return (
-    <div className="expensesCard">
-      <span>€ {amount.toFixed(2).replace(".", ",")}</span>
+
+      <div className="expensesCard">
+        <span>€ {amount.toFixed(2).replace(".", ",")}</span>
     </div>
   );
 };
